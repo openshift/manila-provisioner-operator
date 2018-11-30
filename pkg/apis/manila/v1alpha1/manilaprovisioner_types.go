55dcbf666f0b3f5397a8220e143135167c16867d
Matthew Wong <mawong@redhat.com>
2018-11-30 14:56:21 -0500

    Put parameters in API

--- pkg/apis/manila/v1alpha1/manilaprovisioner_types.go
+++ pkg/apis/manila/v1alpha1/manilaprovisioner_types.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -11,12 +13,81 @@ import (
 type ManilaProvisionerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	operatorv1alpha1.OperatorSpec `json:",inline"`
+
+	// Number of replicas to deploy for a provisioner deployment.
+	// Optional, defaults to 1.
+	Replicas int32 `json:"replicas"`
+
+	// Name of storage class to create. If the storage class already exists, it will not be updated.
+	//// This allows users to create their storage classes in advance.
+	// Required, no default.
+	StorageClassName string `json:"storageClassName"`
+
+	// The reclaim policy of the storage class
+	// Optional, defaults to OpenShift default "Delete."
+	ReclaimPolicy *v1.PersistentVolumeReclaimPolicy `json:"reclaimPolicy,omitempty"`
+
+	// TODO [aos-devel] Attn: All OpenShift 4.0 Components Needing AWS credentials
+	OpenStackSecrets v1.SecretReference
+
+	// Optional, defaults to "default".
+	Type *string `json:"type,omitempty"`
+
+	// Optional, defaults to "nova".
+	Zones []string `json:"zones,omitempty"`
+
+	// Required, no default.
+	Protocol Protocol `json:"protocol"`
+
+	// Required, no default.
+	Backend Backend `json:"backend"`
+
+	// Required, no default.
+	OsSecretName *string `json:"osSecretName"`
+
+	// Optional, defaults to "default".
+	OsSecretNamespace *string `json:"osSecretNamespace,omitempty"`
+
+	// Optional, defaults to value of osSecretNamespace.
+	ShareSecretNamespace *string `json:"shareSecretNamespace,omitempty"`
+
+	// Optional, no default.
+	OsShareID *string `json:"osShareID,omitempty"`
+
+	// Optional, no default.
+	OsShareName *string `json:"osShareID,omitempty"`
+
+	// Optional, no default.
+	OsShareAccessID *string `json:"osShareAccessID,omitempty"`
+
+	// Required for backend csi-cephfs and protocol CEPHFS, no default.
+	CSIDriver *string `json:"csiDriver,omitempty"`
+
+	// Optional for backend nfs and protocol NFS, defaults to "0.0.0.0".
+	NFSShareClient *string `json:"nfsShareClient,omitempty"`
 }
+
+type Protocol string
+
+const (
+	ProtocolCephFS = "CEPHFS"
+	ProtocolNFS    = "NFS"
+)
+
+type Backend string
+
+const (
+	BackendCephFS    = "cephfs"
+	BackendCSICephFS = "csi-cephfs"
+	BackendNFS       = "nfs"
+)
 
 // ManilaProvisionerStatus defines the observed state of ManilaProvisioner
 type ManilaProvisionerStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	operatorv1alpha1.OperatorStatus `json:",inline"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
